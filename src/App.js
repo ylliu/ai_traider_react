fb732dd9ecdbdbaf4677be87c7a71c036fa15acf
yyy <1232423@xx.com>
2024-12-18 17:26:16 +0800

    增加持仓功能 去掉登录功能

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TimeShareChart from "./component/time_share_chart";
 import MonitorStocks from "./component/MonitorStocks";  // 导入 MonitorStocks 组件
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthForm from "./component/AuthForm";  // 导入 Register 组件
-
+import MyHoldings from "./component/MyHoldings";  // 导入 MyHoldings 组件
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -12,18 +12,18 @@ const App = () => {
   };
 
 
-  // 如果用户没有注册，显示注册界面
-  if (!isAuthenticated) {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-12 p-3">
-            <AuthForm onAuthSuccess={handleAuthSuccess} />
-          </div>
-        </div>
-      </div>
-    );
-  }
+  // // 如果用户没有注册，显示注册界面
+  // if (!isAuthenticated) {
+  //   return (
+  //     <div className="container-fluid">
+  //       <div className="row">
+  //         <div className="col-md-12 p-3">
+  //           <AuthForm onAuthSuccess={handleAuthSuccess} />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
  
   return (
@@ -33,10 +33,16 @@ const App = () => {
         <div className="col-md-3 bg-light p-3">
           <MonitorStocks />  {/* 显示 MonitorStocks 组件 */}
         </div>
-        {/* 右侧窗口，包含 TimeShareChart 组件 */}
-        <div className="col-md-9 p-3">
-          <h4>Stock Sell Assistant </h4>
-          { <TimeShareChart /> }
+        
+        {/* 中间窗口，包含 TimeShareChart 组件 */}
+        <div className="col-md-6 p-3">
+          <h4>Stock Sell Assistant</h4>
+          <TimeShareChart /> {/* 显示 TimeShareChart 组件 */}
+        </div>
+  
+        {/* 右侧窗口，包含 MyHoldings 组件 */}
+        <div className="col-md-3 p-3">
+          <MyHoldings />  {/* 显示 MyHoldings 组件 */}
         </div>
       </div>
     </div>
